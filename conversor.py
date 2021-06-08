21a34f4da6f6268b69ea04f225f3667705447351
ikevinmejia <kevin_6958@hotmail.com>
2021-06-07 19:41:21 -0500

    Clase condicionales y ejemplo de conversor de monedas

--- conversor.py
+++ conversor.py
@@ -1,11 +1,39 @@
 #COP vs USD
-""" pesos = float(input("¿Cuántos pesos colombianos tienes?: "))
-valor_dolar = 3875
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $" + dolares + " dólares") """
+menu = """
+Bienvenido al conversor de monedas. 💰
 
+1 - Pesos colombianos.
+2 - Pesos argentinos.
+3 - Pesos mexicanos.
+
+Elige una opción: """
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    pesos = float(input("¿Cuántos pesos colombianos tienes?: "))
+    valor_dolar = 3875
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+elif opcion == 2:
+    pesos = float(input("¿Cuántos pesos argentinos tienes?: "))
+    valor_dolar = 65
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+elif opcion == 3:
+    pesos = float(input("¿Cuántos pesos mexicanos tienes?: "))
+    valor_dolar = 24
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+else:
+    print("Escoge una opción correcta, por favor.")
+    
 #USD vs COP
 
 """ usd = int(input("USD a cambiar: "))
